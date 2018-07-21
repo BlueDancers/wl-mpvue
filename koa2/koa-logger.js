763eea4192332b377ad08e3c6a7130cbefbaf71e
小五 <wu.vkcyan@gmail.com>
2018-07-21 21:17:18 +0800

    no message

--- koa2/koa-logger.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (ctx,next) => {
-  const start = new Date().getTime();
-  await next();
-  const end = new Date().getTime();
-  console.log(ctx.request.url , end-start , ctx.body.length);
-}
-
-//使用koa实现的查看响应时间的中间件
